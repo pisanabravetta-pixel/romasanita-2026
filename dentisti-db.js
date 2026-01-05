d4fd5539ce99a0e393b079de23e6940a8b9c9a9c
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-05 21:18:03 +0100

    Create dentisti-db.js

---
+++ dentisti-db.js
@@ -0,0 +1,34 @@
+export const databaseDentisti = [
+  {
+    id: 1,
+    nome: "Studio Dentistico dr Marco Bazzucchi",
+    zona: "Prati",
+    indirizzo: "Via dei Gracchi, 151",
+    link: "/dentisti-roma-prati",
+    top: true
+  },
+  {
+    id: 2,
+    nome: "Centro Dentistico Cavour",
+    zona: "Prati",
+    indirizzo: "Via Pietro Cossa, 28",
+    link: "/dentisti-roma-prati",
+    top: false
+  },
+  {
+    id: 3,
+    nome: "De Sanctis Odontoiatria Digitale",
+    zona: "Eur",
+    indirizzo: "Viale Europa, 64",
+    link: "/dentisti-roma-eur",
+    top: true
+  },
+  {
+    id: 4,
+    nome: "Studio Giovannini Ludovici",
+    zona: "Eur",
+    indirizzo: "Viale Beethoven, 70",
+    link: "/dentisti-roma-eur",
+    top: false
+  }
+];
