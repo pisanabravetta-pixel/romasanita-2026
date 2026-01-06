2d9272cc2df46578e837a69d3d3c95305ea17546
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-06 21:15:24 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,27 @@ export default function Document() {
   return (
     <Html lang="it">
       <Head>
-        {/* Caricamento Font Inter da Google Fonts */}
+        {/* Caricamento Font Inter da Google Fonts - Ottimizzato per prestazioni */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
+          rel="stylesheet" 
+        />
         
-        {/* Icone FontAwesome */}
+        {/* Icone FontAwesome per i simboli di WhatsApp, Telefono e Mappe */}
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+          crossOrigin="anonymous" 
+          referrerPolicy="no-referrer"
         />
+
+        {/* Suggerimento: Aggiungi qui una favicon per personalizzare la scheda del browser */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
+      <body style={{ margin: 0, padding: 0 }}>
         <Main />
         <NextScript />
       </body>
