367d389c32e388573d0a7f2f77dc839414f82453
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-06 21:14:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import './globals.css' // Ho tolto ../styles/ perché ora il file è nella stessa cartella
+import './globals.css';
 
+/**
+ * Componente principale dell'applicazione (Next.js Custom App)
+ * Utilizzato per inizializzare le pagine e persistere i layout o il CSS globale.
+ */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
