a0db5cd5d233e6df636c0709b325c9fc6fabee6e
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-06 16:29:10 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,2 +1,6 @@
-https://mkmyadztjcnebrhuzdka.supabase.co
-sb_publishable_HetGXnk4p_KXZNOlCAmf3w_SW2iJ_ya
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://mkmyadztjcnebrhuzdka.supabase.co'
+const supabaseAnonKey = 'sb_publishable_HetGXnk4p_KXZNOlCAmf3w_SW2iJ_ya'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
