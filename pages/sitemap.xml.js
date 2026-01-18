2e155524f3f0fbdfdeac8fe77f2b80b6d247fd54
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-18 23:51:16 +0100

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,51 +1,67 @@
-import { seoData } from '../lib/seo-logic';
+import { specialisticheTop, quartieriTop } from '../lib/seo-logic';
 
 const EXTERNAL_DATA_URL = 'https://servizisalute-roma.vercel.app';
 
-// LISTA FILTRATA (Solo le 10 zone principali per iniziare)
-const quartieri = [
-  'prati', 'eur', 'parioli', 'centro-storico', 'san-giovanni', 
-  'monteverde', 'ostia', 'tiburtina', 'aurelio', 'montesacro'
-];
-
-function generateSiteMap(specialistiche) {
-  const date = new Date().toISOString();
-  
+function generateSiteMap(pagine) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url><loc>${EXTERNAL_DATA_URL}/</loc><lastmod>${date}</lastmod><priority>1.0</priority></url>
-     <url><loc>${EXTERNAL_DATA_URL}/servizi-sanitari-roma</loc><lastmod>${date}</lastmod><priority>0.9</priority></url>
-     <url><loc>${EXTERNAL_DATA_URL}/visite-specialistiche-roma</loc><lastmod>${date}</lastmod><priority>0.9</priority></url>
-     <url><loc>${EXTERNAL_DATA_URL}/quartieri-roma</loc><lastmod>${date}</lastmod><priority>0.8</priority></url>
-     <url><loc>${EXTERNAL_DATA_URL}/chi-siamo</loc><lastmod>${date}</lastmod><priority>0.5</priority></url>
-
-     ${Object.keys(specialistiche).map((key) => {
-       return `<url>
-         <loc>${EXTERNAL_DATA_URL}/${key}-roma</loc>
-         <lastmod>${date}</lastmod>
-         <priority>0.8</priority>
-       </url>`;
-     }).join('')}
-
-     ${Object.keys(specialistiche).flatMap((spec) => 
-       quartieri.map((q) => {
-         return `<url>
-           <loc>${EXTERNAL_DATA_URL}/${spec}-roma-${q}</loc>
-           <lastmod>${date}</lastmod>
-           <priority>0.7</priority>
-         </url>`;
+     ${pagine
+       .map(({ url, priority }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}${url}`}</loc>
+           <lastmod>${new Date().toISOString()}</lastmod>
+           <priority>${priority}</priority>
+       </url>
+     `;
        })
-     ).join('')}
+       .join('')}
    </urlset>
  `;
 }
 
+function SiteMap() {
+  // Il browser non userà questo componente, getServerSideProps farà tutto il lavoro
+}
+
 export async function getServerSideProps({ res }) {
-  const sitemap = generateSiteMap(seoData);
+  // 1. PAGINE FISSE
+  const staticPages = [
+    { url: '', priority: 1.0 },
+    { url: '/servizi-sanitari-roma', priority: 0.9 },
+    { url: '/visite-specialistiche-roma', priority: 0.9 },
+    { url: '/quartieri-roma', priority: 0.8 },
+    { url: '/chi-siamo', priority: 0.5 },
+    { url: '/contatti', priority: 0.5 },
+    { url: '/pubblica-annuncio', priority: 0.7 },
+  ];
+
+  // 2. PAGINE HUB (Es: /psicologi-roma, /dentisti-roma)
+  const pagineHub = specialisticheTop.map(cat => ({
+    url: `/${cat}-roma`,
+    priority: 0.8
+  }));
+
+  // 3. PAGINE QUARTIERE (Es: /psicologi-roma-prati)
+  const pagineDinamiche = specialisticheTop.flatMap(cat => 
+    quartieriTop.map(q => ({
+      url: `/${cat}-roma-${q.s}`,
+      priority: 0.7
+    }))
+  );
+
+  // UNISCO TUTTO
+  const tutteLePagine = [...staticPages, ...pagineHub, ...pagineDinamiche];
+
+  const sitemap = generateSiteMap(tutteLePagine);
+
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
-  return { props: {} };
+
+  return {
+    props: {},
+  };
 }
 
-export default function SiteMap() {}
+export default SiteMap;
