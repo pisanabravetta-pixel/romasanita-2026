57796fcf1d5c9418127987ae073301fafb11105e
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-21 00:48:13 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,25 +4,13 @@ export default function Document() {
   return (
     <Html lang="it">
       <Head>
-        {/* Caricamento Font Inter da Google Fonts - Ottimizzato per prestazioni */}
+        {/* Solo i font essenziali: questo evita conflitti con il titolo della Index */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
           rel="stylesheet" 
         />
-        
-        {/* Icone FontAwesome per i simboli di WhatsApp, Telefono e Mappe */}
-        <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
-          crossOrigin="anonymous" 
-          referrerPolicy="no-referrer"
-        />
-
-        {/* Suggerimento: Aggiungi qui una favicon per personalizzare la scheda del browser */}
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
