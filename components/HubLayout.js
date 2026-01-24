1349f15161f39d955d28d1e06582f76cdcbb18f1
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-24 23:56:17 +0100

    Update HubLayout.js

--- components/HubLayout.js
+++ components/HubLayout.js
@@ -137,7 +137,7 @@ export default function HubLayout({
   loading="lazy"
   src={`https://www.google.com/maps?q=${encodeURIComponent(
     medici && medici.length > 0 
-      ? medici.slice(0, 8).map(m => `"${m.nome} Roma"`).join(' OR ') 
+      ? medici.map(m => `"${m.nome}"`).join(' OR ') 
       : "Roma"
   )}&t=&z=11&ie=UTF8&iwloc=B&output=embed`}
 ></iframe>
