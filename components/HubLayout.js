ee25ac697731941c3eca45c2af027600a0824ca4
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-25 01:08:37 +0100

    Update HubLayout.js

--- components/HubLayout.js
+++ components/HubLayout.js
@@ -126,7 +126,7 @@ export default function HubLayout({
   </div>
 </div>
 
-{/* BOX MAPPA HUB - IL TUO CODICE ORIGINALE RIPRISTINATO */}
+{/* BOX MAPPA HUB - FIX BUILD E PULIZIA ESTRANEI */}
 <div style={{ marginBottom: '30px' }}>
   <div style={{ width: '100%', height: '350px', borderRadius: '12px', overflow: 'hidden', border: '1px solid #e2e8f0' }}>
     {medici && medici.length > 0 ? (
@@ -137,7 +137,7 @@ export default function HubLayout({
         loading="lazy"
         src={`https://maps.google.com/maps?q=${encodeURIComponent(
           medici.filter(m => m.indirizzo).length > 0
-            ? medici.filter(m => m.indirizzo).map(m => `"${m.nome} ${m.indirizzo}"`).join(' OR ') // Qui ho aggiunto m.nome per precisione
+            ? medici.filter(m => m.indirizzo).map(m => `"${m.indirizzo}"`).join(' OR ')
             : "Roma, Italia"
         )}&t=&z=11&ie=UTF8&iwloc=B&output=embed`}
       ></iframe>
