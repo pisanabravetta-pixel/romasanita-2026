327f4d8104fd7e2b4fb0c66b378e8c170cd78810
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-27 22:31:22 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,6 @@ export default function Document() {
   return (
     <Html lang="it">
       <Head>
-        {/* Solo i font essenziali: questo evita conflitti con il titolo della Index */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
-          rel="stylesheet" 
-        />
-       import { Html, Head, Main, NextScript } from 'next/document'
-
-export default function Document() {
-  return (
-    <Html lang="it">
-      <Head>
         {/* Solo i font essenziali */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -28,14 +15,6 @@ export default function Document() {
         {/* LIBRERIA LEAFLET PER LA MAPPA */}
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" async></script>
-      </Head>
-      <body style={{ margin: 0, padding: 0 }}>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}     
       </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
