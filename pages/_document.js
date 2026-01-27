f72e13a3fd556c702c510c1af9238cdb9d8d1e44
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-01-27 22:29:04 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,31 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
           rel="stylesheet" 
         />
+       import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="it">
+      <Head>
+        {/* Solo i font essenziali */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
+          rel="stylesheet" 
+        />
+
+        {/* LIBRERIA LEAFLET PER LA MAPPA */}
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" async></script>
+      </Head>
+      <body style={{ margin: 0, padding: 0 }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}     
       </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
