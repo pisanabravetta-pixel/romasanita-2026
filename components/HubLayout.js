27f7640fd84824800fc5672fd7615ff1f2306282
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-06 22:07:20 +0100

    Update HubLayout.js

--- components/HubLayout.js
+++ components/HubLayout.js
@@ -27,7 +27,7 @@ const [serviziRealTime, setServiziRealTime] = useState([]);
   const annunciPerPagina = 10;
 
   const totaleAnnunci = (serviziRealTime || []).length;
-  const totalePagine = Math.max(1, Math.ceil(totaleAnnunci / annunciPerPagina));
+  const totalePagine = Math.ceil(totaleAnnunci / annunciPerPagina) || 1;
   
   const inizio = (pagina - 1) * annunciPerPagina;
   const listaDaMostrare = (serviziRealTime || []).slice(inizio, inizio + annunciPerPagina);
