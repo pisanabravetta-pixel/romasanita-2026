871cab394804a38f79a3907c452f3b0ef1251b5b
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-10 23:39:01 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+return (
+<>
+
+<Script
+src=""
+strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+{window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(&#39;js&#39;, new Date()); gtag(&#39;config&#39;, &#39;G-2CNB67FYJG&#39;);}
+</Script>
+
+<Component {...pageProps} />
+</>
+);
 }
 
 export default MyApp;
