03b35af963eba9f3ded626cdbda50aeddd384103
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-12 21:09:53 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,22 @@ import '../styles/globals.css';
 import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-const googleCode = "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2CNB67FYJG');";
+  const googleCode = "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2CNB67FYJG');";
 
-return (
-<>
-<Script
-src=""
-strategy="afterInteractive"
-/>
-<Script id="google-analytics" strategy="afterInteractive">
-{googleCode}
-</Script>
-<Component {...pageProps} />
-</>
-);
+  return (
+    <>
+      {/* Script principale di Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-2CNB67FYJG"
+        strategy="afterInteractive"
+      />
+      {/* Configurazione dello script */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {googleCode}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
