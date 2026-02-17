782b7ab67e4d1bc9fc12d954f479c6e4cd79f677
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-17 21:55:31 +0100

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -381,7 +381,7 @@ setMeta({
         
         {/* WHATSAPP FISSO E SEMPRE VERDE */}
         <a 
-          href={v.whatsapp ? `https://wa.me/39${String(v.whatsapp).replace(/\D/g, '').replace(/^39/, '')}?text=${encodeURIComponent(`Salve, la contatto tramite ServiziSalute.com per informazioni su: ${v.nome}`)}` : '#'}
+         href={v.whatsapp ? `https://wa.me/39${String(v.whatsapp).replace(/\D/g, '').replace(/^39/, '')}?text=${encodeURIComponent(`Salve, la contatto perché ho visto il suo annuncio su ServiziSalute.com`)}` : '#'}
           onClick={(e) => { 
             if(!v.whatsapp) { 
               e.preventDefault(); 
