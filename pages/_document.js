31f863bdad68f90908db8034e34655df660690dd
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-20 00:56:29 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -22,11 +22,12 @@ export default function Document() {
         <NextScript />
  
      <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
-          media="print" 
-          onLoad="this.media='all'" 
-        />
+  rel="stylesheet" 
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+  media="print" 
+  onLoad="this.media='all'" 
+  crossOrigin="anonymous" 
+/>
       </body>
     </Html>
   )
