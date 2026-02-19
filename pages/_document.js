981a7e55279d5cc6c02c9d3f431ba48b4509b681
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-20 00:32:37 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,17 @@ export default function Document() {
   <link rel="preconnect" href="https://images.unsplash.com" /> {/* Aggiungilo qui */}
 
   <link 
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
-    rel="stylesheet" 
-  />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />    
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
+  rel="stylesheet" 
+  media="print" 
+  onLoad="this.media='all'" 
+/>
+  <link 
+  rel="stylesheet" 
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+  media="print" 
+  onLoad="this.media='all'" 
+/>
 </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
