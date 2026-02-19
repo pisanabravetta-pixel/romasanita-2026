bbc1a5114b13daaa5d90d53e5802357c336b9cc8
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-20 00:21:32 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default function Document() {
     href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
     rel="stylesheet" 
   />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />    
 </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
