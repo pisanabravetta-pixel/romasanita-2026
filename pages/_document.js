7e2b73dc312ffe231a6e2b6955ab3a7bc010aceb
pisanabravetta-pixel <pisanabravetta@gmail.com>
2026-02-20 00:48:53 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -15,12 +15,68 @@ export default function Document() {
   media="print" 
   onLoad="this.media='all'" 
 />
+ import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="it">
+     <Head>
+  {/* Preconnessioni centralizzate (UNA sola volta per tutto il sito) */}
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+  <link rel="preconnect" href="https://images.unsplash.com" /> {/* Aggiungilo qui */}
+
+  <link 
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
+  rel="stylesheet" 
+  media="print" 
+  onLoad="this.media='all'" 
+/>
   <link 
   rel="stylesheet" 
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
   media="print" 
   onLoad="this.media='all'" 
 />
+</Head>
+      <body style={{ margin: 0, padding: 0 }}>
+        <Main />
+        <NextScript />
+  import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="it">
+     <Head>
+  {/* Preconnessioni centralizzate (UNA sola volta per tutto il sito) */}
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+  <link rel="preconnect" href="https://images.unsplash.com" /> {/* Aggiungilo qui */}
+
+  <link 
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" 
+  rel="stylesheet" 
+  media="print" 
+  onLoad="this.media='all'" 
+/>
+  <link 
+  rel="stylesheet" 
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+  media="print" 
+  onLoad="this.media='all'" 
+/>
+</Head>
+      <body style={{ margin: 0, padding: 0 }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+      </body>
+    </Html>
+  )
+}
 </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <Main />
